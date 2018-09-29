5f87ec8508b48949e84f12d55cebfdb9ecdad44d
pastapojken <pastapojken@users.noreply.github.com>
2018-09-29 18:33:15 +0200

    Updated with "sort by name" at end of script

---
+++ scripts/sortByName.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "${0}: usage: sortByName.sh (1 for movies | 2 for tv shows | 3 for both )"
+    exit 1
+fi
+
+cd "$(dirname "$0")"
+dbNameMovie="../dbM.json" # identifies path and name of the movie database
+dbNameTV="../dbTV.json" # identifies path and name of the tv show database
+. config.cfg
+
+case "${1}" in #switch case for the program's argument
+    "1")
+		cat $dbNameMovie | jq 'sort_by(.Movie)' -r > $dbNameMovie;
+        ;;
+    "2")
+		cat $dbNameMovie | jq 'sort_by(.Show)' -r > $dbNameMovie;
+        ;;
+    "3")
+		cat $dbNameMovie | jq 'sort_by(.Movie)' -r > $dbNameMovie;
+		cat $dbNameMovie | jq 'sort_by(.Show)' -r > $dbNameMovie;
+        ;;
+    *)
+        echo "Invalid input, use: #1 for only movies, #2 for only tv shows and #3 for both";;
+esac
+echo "Sorted DBs"
+
