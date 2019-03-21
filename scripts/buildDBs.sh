8f5d5b94ed70fd15012fc27af516da95e4560d72
pastapojken <pastapojken@users.noreply.github.com>
2019-03-21 11:51:34 +0100

    Update buildDBs.sh

--- scripts/buildDBs.sh
+++ scripts/buildDBs.sh
@@ -14,7 +14,7 @@ case "${1}" in #switch case for the program's argument
 	"1")
 		#find all files that end in mp4 and executes parseMfilename.sh for every one of them, sequentially
 		find $MoviesPath  -not -path '*/.*/*' -not -name '.*' -iname "*.mp4" -exec ./parseMfilename.sh {} \;
-		./sortByName.sh ${1}
+		./sortByName.sh ${1};
 		;;
 	"2")
 		find $TVpath -not -path '*/.*/*' -not -name '.*' -name "*.mp4"| sort | while read file; do #sorts filenames and iterates through them
@@ -34,16 +34,16 @@ case "${1}" in #switch case for the program's argument
 		pid1=$!
 		find $TVpath -not -path '*/.*/*' -not -name '.*' -name "*.mp4"| sort | while read file; do #see above
 			if [ ! -f $dbNameTV ]; then #creates the dbfile if missing
-            	touch $dbNameTV;
-            fi
+				touch $dbNameTV;
+			fi
 			fileName=${file#../}
-            isPresent=$(grep -c "${fileName}" $dbNameTV)
-            if [ "$isPresent" -eq "0" ]; then #check if file is already present in database
-            	./parseTVfilename.sh $file	#parses through $file, adds it to database
-            fi
+			isPresent=$(grep -c "${fileName}" $dbNameTV)
+			if [ "$isPresent" -eq "0" ]; then #check if file is already present in database
+				./parseTVfilename.sh $file	#parses through $file, adds it to database
+			fi
 		done
 		wait $pid1;
-		./sortByName.sh ${1}
+		./sortByName.sh ${1};
 		;;
 	*)
 		echo "Invalid input, use: #1 for only movies, #2 for only tv shows and #3 for both";;
